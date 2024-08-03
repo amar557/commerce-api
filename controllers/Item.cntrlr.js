708fe7b08380e69929ef6295109844e872995e21
amar ali <amarhussain391@gmail.com>
2024-08-04 04:38:13 +0500

    commited

--- controllers/Item.cntrlr.js
+++ controllers/Item.cntrlr.js
@@ -2,10 +2,14 @@ import Item from "../schemas/Item.schema.js";
 import order from "../schemas/order.schema.js";
 
 export const uploadItem = async function (req, res, next) {
-  const item = req.body;
-  const upladedItem = await Item(item);
-  upladedItem.save();
-  res.send(upladedItem);
+  try {
+    const item = req.body;
+    const upladedItem = await Item(item);
+    upladedItem.save();
+    res.send(upladedItem);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 };
 
 export const getItems = async function (req, res, next) {
